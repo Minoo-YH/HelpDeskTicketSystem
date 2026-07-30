5aa20058e9e327f71e0cd1014a8d3ad4e936cf09
Minoo Yaghoubi <155952014+Minoo-YH@users.noreply.github.com>
2026-07-30 13:01:04 +0300

    Configure Entity Framework Core and database context

--- HelpDesk.API/Program.cs
+++ HelpDesk.API/Program.cs
@@ -1,15 +1,18 @@
+using HelpDesk.API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 builder.Services.AddControllers();
 
-// Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
