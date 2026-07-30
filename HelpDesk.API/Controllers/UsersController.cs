9ce4724ffe183bb59d7d498a666cb200232bd972
Minoo Yaghoubi <155952014+Minoo-YH@users.noreply.github.com>
2026-07-30 13:40:27 +0300

    Add user login endpoint

--- HelpDesk.API/Controllers/UsersController.cs
+++ HelpDesk.API/Controllers/UsersController.cs
@@ -2,29 +2,30 @@ using HelpDesk.API.Data;
 using HelpDesk.API.DTOs;
 using HelpDesk.API.Models;
 using Microsoft.AspNetCore.Mvc;
-
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
 namespace HelpDesk.API.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
 public class UsersController : ControllerBase
 {
-    private readonly ApplicationDbContext _context;
+  private readonly ApplicationDbContext _context;
 
-    public UsersController(ApplicationDbContext context)
-    {
-        _context = context;
-    }
+  public UsersController(ApplicationDbContext context)
+  {
+    _context = context;
+  }
 
   [HttpPost]
-public async Task<IActionResult> Register(RegisterRequest request)
-{
+  public async Task<IActionResult> Register(RegisterRequest request)
+  {
     var user = new User
     {
-        FullName = request.FullName,
-        Email = request.Email,
-        PasswordHash = BCrypt.Net.BCrypt.HashPassword(request.Password),
-        CreatedAt = DateTime.UtcNow
+      FullName = request.FullName,
+      Email = request.Email,
+      PasswordHash = BCrypt.Net.BCrypt.HashPassword(request.Password),
+      CreatedAt = DateTime.UtcNow
     };
 
     _context.Users.Add(user);
@@ -32,5 +33,26 @@ public async Task<IActionResult> Register(RegisterRequest request)
     await _context.SaveChangesAsync();
 
     return Ok("User registered successfully.");
-}
+  }
+  [HttpPost("login")]
+  public IActionResult Login(LoginRequest request)
+  {
+    var user = _context.Users.FirstOrDefault(x => x.Email == request.Email);
+
+    if (user == null)
+    {
+      return Unauthorized("Invalid email or password.");
+    }
+
+    bool isPasswordValid = BCrypt.Net.BCrypt.Verify(
+        request.Password,
+        user.PasswordHash);
+
+    if (!isPasswordValid)
+    {
+      return Unauthorized("Invalid email or password.");
+    }
+
+    return Ok("Login successful.");
+  }
 }
